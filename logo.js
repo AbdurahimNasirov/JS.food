b0103f35b70269e4aa849f7be5a3a50344db1cde
Abdurahim <abdurahmonnosirov115@gmail.com>
2022-02-01 00:34:21 +0500

    :)

--- logo.js
+++ logo.js
@@ -1,18 +1,197 @@
 const logoNumber = document.querySelector(".header__timer-extra");
 
 const changeNumber = () => {
+
   let time;
+
   let currentNum = logoNumber.innerHTML
+
   if (currentNum >= 98) time = 1000;
+
   else if (currentNum > 95) time = 300;
+
   else if (currentNum > 85) time = 200;
+
   else if (currentNum > 75) time = 150;
+
   else if (currentNum > 50) time = 75;
+
   else time = 50;
 
   logoNumber.innerHTML++;
+
   if (logoNumber.innerHTML >= 100) return;
+
   setTimeout(changeNumber, time);
 };
 
 changeNumber();
+
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
+// a
